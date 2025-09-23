6bbd01adea9075643fff3a7c85992aa7edc04b59
Sai Pavan Bharath (LTIMindtree Limited) <v-sabharath@microsoft.com>
2025-09-23 16:26:32 -0700

    some message

--- 06-conditions.sh
+++ 06-conditions.sh
@@ -3,8 +3,7 @@
 NUMBER=$!
 
 if [ $NUMBER -lt 10 ]; then
-    echo "Given number $NUMBER is less than 10"
+    echo "Given number $NUMBER is less than 10"   
 else
     echo "Given number $NUMBER is greater than or equal to 10"
 fi
-    
