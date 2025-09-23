3458a082026c6cbb3f7fd0de7994315b750cca83
Sai Pavan Bharath (LTIMindtree Limited) <v-sabharath@microsoft.com>
2025-09-23 14:50:07 -0700

    some message

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh:: Hi Suresh, How are you?"
+echo "Suresh:: Hello Ramesh, I an good How are you?"
+echo "Ramesh:: That's glad to hear, What are you upto now a days?"
+echo "Suresh:: I'm learning shell-scripting"
