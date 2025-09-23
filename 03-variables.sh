6dee9af2f7913d83c70dfdec707c40d558fd3c42
Sai Pavan Bharath (LTIMindtree Limited) <v-sabharath@microsoft.com>
2025-09-23 14:58:57 -0700

    some message

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=Michael
+PERSON2=Jordon
+
+echo "$PERSON1:: Hi $PERSON2, How are you?"
+echo "$PERSON2:: Hello $PERSON2, I an good How are you?"
+echo "$PERSON1:: That's glad to hear, What are you upto now a days?"
+echo "PERSON2:: I'm learning shell-scripting"
